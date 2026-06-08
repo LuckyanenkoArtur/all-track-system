507821dede684425a6e91c3586bf20a9575a2b42
Arthur Lukyanenko <lukyanenko@gmail.com>
2026-06-08 11:52:38 -0700

    Add the Tasks page

--- src/features/calendar/index.tsx
+++ src/features/calendar/index.tsx
@@ -1,3 +1,6 @@
 export default function CalendarPage() {
+  // here we have the calender that will be display tasks need to be done, can be display spendinds past and in the fueture
+  // We can switch view of calander, day, weeks, 2weeks, month, 2 month, 3month
+
   return <h1>DashBoard We Display mains</h1>;
 }
