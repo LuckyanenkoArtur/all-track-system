e84361012b9cfc1386cb7ed7dcfae2280fbb6d59
Arthur <lukyanena@gmail.com>
2026-06-28 08:58:53 +0300

    [refactor: I18n refactoring]

--- src/i18n/domain/LanguageConfig.ts
+++ src/i18n/domain/LanguageConfig.ts
@@ -3,16 +3,16 @@ export const SUPPORTED_LANGUAGES = ["en", "ru"] as const;
 export type Language = (typeof SUPPORTED_LANGUAGES)[number];
 
 export const LanguageConfig = {
-  supportedLanguages: SUPPORTED_LANGUAGES,
-  defaultLanguage: "en" as Language,
-  preferencesStorageKey: "alltrack-preferences",
-  detection: {
-    customDetectorName: "allTrackPreferences",
-    order: ["allTrackPreferences", "navigator"],
-    caches: ["allTrackPreferences"],
-  },
-  translation: {
-    namespace: "translation",
-    loadPath: "/lang/{{lng}}.json",
-  },
+    supportedLanguages: SUPPORTED_LANGUAGES,
+    defaultLanguage: "en" as Language,
+    preferencesStorageKey: "alltrack-preferences",
+    detection: {
+        customDetectorName: "allTrackPreferences",
+        order: ["allTrackPreferences", "navigator"],
+        caches: ["allTrackPreferences"],
+    },
+    translation: {
+        namespace: "translation",
+        loadPath: "/lang/{{lng}}.json",
+    },
 } as const;
